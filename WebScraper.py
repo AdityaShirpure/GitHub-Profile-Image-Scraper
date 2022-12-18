0c51b7b6cc1b0e12d01bb90f515731cdbd216398
AdityaShirpure <Shirpure98@gmail.com>
2022-12-18 22:51:22 +0530

    Project completed

---
+++ WebScraper.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup as bs
+
+# Assigning a variable to collect that Github user
+github_user = input('Input the Github User: ')
+
+# Sending request to URL 
+url = 'https://github.com/' + github_user 
+req = requests.get(url)
+
+# Check if the request was successful
+if req.status_code == 200:
+  soup = bs(req.content,'html.parser')
+  profile_image = soup.find('img',{'alt':'Avatar'})
+  if profile_image:
+    image_src = profile_image['src']
+    print(image_src)
+  else:
+    print("Profile image not found.")
+else:
+  print("Invalid username or URL.")
